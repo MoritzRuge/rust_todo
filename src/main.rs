46fe3f079da34972cad96fabfd5cb830c03646c0
Moritz <ruge.moritz@gmail.com>
2026-04-24 15:43:01 +0200

    some testing to draw a layout

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use crossterm::event::{self, KeyCode};
+use ratatui::layout::{Constraint, Direction, Layout};
+use ratatui::widgets::{Block, Borders, List, ListItem, Paragraph};
 use ratatui::{DefaultTerminal, Frame};
+
+struct App {
+    todos: Vec<String>,
+    selected: usize,
+    //completed: Vec<Todo>,
+}
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
@@ -9,12 +18,33 @@ fn main() -> color_eyre::Result<()> {
 fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
     loop {
         terminal.draw(render)?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
+        if let Some(key) = event::read()?.as_key_press_event() {
+            match key.code {
+                KeyCode::Char('q') | KeyCode::Esc => return Ok(()),
+                _ => {}
+            }
         }
     }
 }
 
 fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
+    let outer_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(frame.area());
+
+    let inner_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(outer_layout[0]);
+
+    let items = vec![ListItem::new("Test1"), ListItem::new("Test2")];
+
+    let list = List::new(items);
+    let block = Block::default().title("Mein Block").borders(Borders::ALL);
+    let p = Paragraph::new("Hello, World!").block(Block::new().borders(Borders::ALL));
+
+    frame.render_widget(block, outer_layout[0]);
+    frame.render_widget(list, inner_layout[0]);
+    frame.render_widget(p, outer_layout[1]);
 }
